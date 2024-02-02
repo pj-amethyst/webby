18897caf9c77afd95a47425c6812cd42dc289e9a
floof <rglyxr@gmail.com>
2024-02-02 13:08:14 -0600

    animation of amethyst

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,68 @@
 "use client";
-
+import { animated } from "@react-spring/three";
+import { useSpring, easings } from "@react-spring/web";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import { Koulen } from "next/font/google";
+
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/Addons.js";
+
+function Amethyst(props: any) {
+	console.log(props);
+
+	const fbx = useLoader(FBXLoader, "/source/Amethyst.fbx");
+	return <primitive object={fbx} />;
+}
+
+const koulen = Koulen({
+	weight: "400",
+	subsets: ["latin"],
+});
 
 export default function Home() {
+	const [canvasExists, setCanvasExists] = useState(false);
+
+	const { rotate, scale } = useSpring({
+		rotate: canvasExists ? (Math.PI * 3) / 2 : 0,
+		scale: canvasExists ? 0.05 : 0,
+		config: {
+			duration: 2500,
+			easing: easings.easeOutExpo,
+		},
+		onChange: (event) => {
+			console.log(event);
+		},
+	});
+
 	return (
 		<>
-			<div className="absolute top-0 left-0 -z-10 h-full w-full">
-				<Canvas>
-					<ambientLight intensity={0.1} />
-					<directionalLight color="purple" position={[0, 0, 5]} />
-					<mesh>
-						<boxGeometry />
-						<meshStandardMaterial />
-					</mesh>
+			<div className="absolute top-0 left-0 opacity-75 -z-10 h-full w-full">
+				<Canvas
+					onCreated={(state) => {
+						setCanvasExists(true);
+					}}
+				>
+					<ambientLight intensity={2} />
+					<animated.mesh
+						rotation-y={rotate}
+						scale={scale}
+						position={[0, -2, 0]}
+					>
+						<Amethyst canvasExists />
+					</animated.mesh>
 				</Canvas>
 			</div>
-			<div className="h-screen grid place-items-center">
-				<h1 className="text-6xl tracking-wide">
-					project<span className="text-purple-400">amethyst</span>
-				</h1>
-			</div>
+			{canvasExists && (
+				<div className="h-screen grid place-items-center">
+					<h1 className={"text-6xl " + koulen.className}>
+						project
+						<span className="text-purple-400 tracking-wide">
+							amethyst
+						</span>
+					</h1>
+				</div>
+			)}
 		</>
 	);
 }
